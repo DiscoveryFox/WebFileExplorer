0105226c94a3a26d51e8b6a7a6cd343fba6350a7
discovery-bit <61625792+discovery-bit@users.noreply.github.com>
2022-06-14 21:14:46 +0200

    added function to restore from tempdir and docstring to save_to_tmpdir

--- temp_manager.py
+++ temp_manager.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import tempfile
+import platform
 
 
 class TempManager:
@@ -15,11 +16,23 @@ class TempManager:
 
     # todo: write handlers for all errors that can occur on file operations
 
-    def save_to_tmpdir(self, file_name: str):
+    def save_to_tmpdir(self, file_name: str) -> tuple[bool, str]:
+        """
+        Saves a file to the temporary directory. And deletes it from the current directory.
+        :param file_name: str
+        :return: tuple[bool, str]
+        """
         full_path = os.path.abspath(file_name)
         if os.path.isfile(full_path):
             try:
-                shutil.copy(full_path, f'{self.temp_dir}/{file_name}')
+                print(platform.system())
+                if platform.system() == 'Windows':
+                    shutil.copy(full_path, f'{self.temp_dir}\\{file_name}')
+                    print(f'{full_path} -> {self.temp_dir}\\{file_name}')
+                else:
+                    shutil.copy(full_path, f'{self.temp_dir}/{file_name}')
+                    print(f'{full_path} -> {self.temp_dir}/{file_name}')
+                os.remove(full_path)
             except FileNotFoundError:
                 return True, 'File not found'
             except PermissionError:
@@ -28,8 +41,24 @@ class TempManager:
         elif os.path.isdir(full_path):
             try:
                 shutil.copytree(full_path, f'{self.temp_dir}/{file_name}')
+                shutil.rmtree(full_path)
             except FileNotFoundError:
                 return True, 'File not found'
             except PermissionError:
                 return True, 'PermissionError on rename'
             return False, ''
+
+    def restore_from_tmpdir(self, file_name: str) -> tuple[bool, str]:
+        """
+        Restores a file from the temporary directory. And deletes it from the temporary directory.
+        :param file_name: str
+        :return: tuple[bool, str]
+        """
+        try:
+            shutil.copy(f'{self.temp_dir}/{file_name}', os.getcwd())
+            os.remove(f'{self.temp_dir}/{file_name}')
+        except FileNotFoundError:
+            return True, 'File not found'
+        except PermissionError:
+            return True, 'PermissionError on rename'
+        return False, ''
